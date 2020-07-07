91516b8e10bde434f6e3829078f0c4ace52d3658
Cusatelli <github.cusatelli@gmail.com>
2020-07-07 13:11:24 -0700

    Commented Code in Test.cpp

--- Lumberjack/Lumberjack/Test/Test.cpp
+++ Lumberjack/Lumberjack/Test/Test.cpp
@@ -1,12 +1,16 @@
-
+/*
+ * author: Cusatelli
+ * source: https://github.com/Cusatelli/Luminate
+ * license: MIT License
+ */
 #include "../Lumberjack.h"
 
 int main()
 {
-	Lumberjack debug;
-	debug.Log("This is a Debug.Log(str) test");
-	debug.Log(SEVERITY::ERROR, "Test Print");
-	debug.Log(SEVERITY::CRITICAL, "Test Print");
-	debug.Log(SEVERITY::WARNING, "Test Print");
-	debug.Log(SEVERITY::INFO, "Test Print");
+	Lumberjack debug;								// Initialise Lumberjack Class.
+	debug.Log("This is a Debug.Log(str) test");		// Print a default message in console.
+	debug.Log(SEVERITY::ERROR, "Test Print");		// Print an Error message in console.
+	debug.Log(SEVERITY::CRITICAL, "Test Print");	// Print a Critical message in console.
+	debug.Log(SEVERITY::WARNING, "Test Print");		// Print a Warning message in console.
+	debug.Log(SEVERITY::INFO, "Test Print");		// Print a Info message in console.
 }
