4aae7f6ea26d11c282f43d4793c42bb80921702f
Cusatelli <github.cusatelli@gmail.com>
2020-07-07 13:03:29 -0700

    Commented Code in Severity.h

--- Lumberjack/Lumberjack/Severity/Severity.h
+++ Lumberjack/Lumberjack/Severity/Severity.h
@@ -1,3 +1,8 @@
+/*
+ * author: Cusatelli
+ * source: https://github.com/Cusatelli/Luminate
+ * license: MIT License
+ */
 #pragma once
 
 #include <iostream>
@@ -5,11 +10,12 @@
 class Severity
 {
 public:
-	enum class Level { ERROR = 0, CRITICAL, WARNING, INFO };
+	enum class Level { ERROR = 0, CRITICAL, WARNING, INFO }; // Different Severity Levels.
+	// Get Prefix should be private. Work in progress for a workaround.
 	static std::string GetSeverityPrefix(Level severity_level);
 
 private:
-	Level m_Level = Level::INFO;
+	Level m_Level = Level::INFO; // Default Severity Level.
 	void SetSeverityLevelValue(Level severity_level);
 	int GetSeverityLevelValue();
 };
