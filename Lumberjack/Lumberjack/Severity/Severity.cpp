6abf1ececa7f54a41d7f5ad753baba15f901ba89
Cusatelli <github.cusatelli@gmail.com>
2020-07-07 13:01:35 -0700

    Commented Code in Severity.cpp

--- Lumberjack/Lumberjack/Severity/Severity.cpp
+++ Lumberjack/Lumberjack/Severity/Severity.cpp
@@ -1,15 +1,31 @@
+/*
+ * author: Cusatelli
+ * source: https://github.com/Cusatelli/Luminate
+ * license: MIT License
+ */
 #include "Severity.h"
 
+/*
+ * Set the local Severity Level
+ */
 void Severity::SetSeverityLevelValue(Severity::Level severity_level)
 {
 	m_Level = severity_level;
 }
 
+/*
+ * Get the integer value of the enumeration.
+ * This can be used to identify and filter the log level.
+ */
 int Severity::GetSeverityLevelValue()
 {
 	return static_cast<int>(m_Level);
 }
 
+/*
+ * Get the prefix for logging in console. I.E when logging a message of Warning
+ * Print [ WARNING ]: before the rest of the message.
+ */
 std::string Severity::GetSeverityPrefix(Severity::Level severity_level)
 {
 	switch (severity_level)
