8b9c3f944dfd2498e2419d4a8ce3b543120695f9
Cusatelli <github.cusatelli@gmail.com>
2020-07-07 13:08:22 -0700

    Commented Code in Lumberjack.cpp

--- Lumberjack/Lumberjack/Lumberjack.cpp
+++ Lumberjack/Lumberjack/Lumberjack.cpp
@@ -1,15 +1,33 @@
+/*
+ * author: Cusatelli
+ * source: https://github.com/Cusatelli/Luminate
+ * license: MIT License
+ */
 #include "Lumberjack.h"
 
+/*
+ * Log in console using the default colors.
+ */
 void Lumberjack::Log(std::string str)
 {
 	std::cout << "\033[0m" << str << "\033[0m" << std::endl;
 }
 
+/*
+ * Log in console using custom console colors depending on Severity Levels.
+ * I.E. Severity Level Warning will be printed as yellow. Promptly reset back to
+ * default values at the end of console-out operation.
+ */
 void Lumberjack::Log(SEVERITY severity_level, std::string str)
 {
 	std::cout << GetColor(severity_level) << Severity::GetSeverityPrefix(severity_level) << str << ResetColor() << std::endl;
 }
 
+/*
+ * Return the ANSI color values back in the form of a string value,
+ * depending on Severity Levels the color value will change.
+ * I.E. CRITICAL will return a highlighted RED color.
+ */
 std::string Lumberjack::GetColor(SEVERITY severity_level)
 {
 	switch (severity_level)
@@ -27,6 +45,10 @@ std::string Lumberjack::GetColor(SEVERITY severity_level)
 	}
 }
 
+/*
+ * Return the default ANSI color values back in the form of a string value.
+ * This can be used to reset the previously set color value, back to default.
+ */
 std::string Lumberjack::ResetColor()
 {
 	return "\033[0m";
